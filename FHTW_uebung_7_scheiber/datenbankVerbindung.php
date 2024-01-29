88579a78950505e0c5b11e70e536b6f01ebaca0c
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-01-29 15:51:12 +0100

    changes.

--- FHTW_uebung_7_scheiber/datenbankVerbindung.php
+++ FHTW_uebung_7_scheiber/datenbankVerbindung.php
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 <?php
 $db = mysqli_connect("localhost", "root", "", "studenten");
 if(!$db)
 {
   exit("Verbindungsfehler: ".mysqli_connect_error());
 }
+=======
+<?php
+$db = mysqli_connect("localhost", "root", "", "studenten");
+if(!$db)
+{
+  exit("Verbindungsfehler: ".mysqli_connect_error());
+}
+>>>>>>> b2bc130b264c957c8315f76d6ea36a0a65ee08ac
 ?>
